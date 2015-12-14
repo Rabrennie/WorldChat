03d33c5355f118b184634823b1e84e604dcf873a
Rab Rennie <rab.rennie@planysdigital.com>
2015-12-14 22:18:02 +0000

    fixed bug with messages that don't have a type

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function initMap() {
     minZoom: 2
   });
 
-myInfowindow = new google.maps.InfoWindow({map: map});
+  myInfowindow = new google.maps.InfoWindow({map: map});
 
-myPos = {lat:0,lng:0};
+  myPos = {lat:0,lng:0};
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -30,10 +30,10 @@ myPos = {lat:0,lng:0};
       myInfowindow.setContent('<div id="messageTabs"><span id="textButton" onclick="text()">Text</span><span id="drawButton" onclick="draw()">Draw</span><div><div id="messageArea"><textarea id="message"></textarea><button onclick="saveMessage()">Save</button><div>');
       map.setCenter(myPos);
       myMarker = new google.maps.Marker({
-    position: myPos,
-    map: map,
-  });
-  myInfowindow.open(map,myMarker);
+        position: myPos,
+        map: map,
+      });
+      myInfowindow.open(map,myMarker);
 
 
 
@@ -54,11 +54,11 @@ myPos = {lat:0,lng:0};
   });
 
   myFirebaseRef.on("child_added", function(snapshot) {
-  console.log(snapshot.val());
-  newMessage(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+    console.log(snapshot.val());
+    newMessage(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
 }
 
 function placeMarker(location) {
@@ -78,7 +78,7 @@ function placeMarker(location) {
 
   myMarker.addListener('click', function() {
     myInfowindow.open(map,myMarker);
-  document.getElementById('message').focus()
+    document.getElementById('message').focus()
   });
 }
 
@@ -87,76 +87,76 @@ function placeMarker(location) {
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}
-
-function saveMessage(){
-  var pos = myPos
-  if(mode =='text'){
-  myFirebaseRef.push({
-    type:'text',
-  message: _.escape(document.getElementById('message').value),
-  location: pos
-});
-} else {
-  imageData = $('#simple_sketch')[0].toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, "");
-  $.ajax({
-    url: 'https://api.imgur.com/3/image',
-    headers: {
-        'Authorization': 'Client-ID a2bb58386e94eb0'
-    },
-    type: 'POST',
-    data: {
-        'image': imageData
-    },
-    success: function(data) { myFirebaseRef.push({
-      type:'image',
-    message: data.data.link,
-    location: pos
-  }); }
-});
-}
-myMarker.setMap(null)
-}
-function draw(){
-  $('#messageArea').html('<canvas id="simple_sketch" width="300" height="100"></canvas><button onclick="saveMessage()">Save</button>')
-  $(function() {
-    $('#simple_sketch').sketch();
-  });
-  mode = 'draw';
-}
-function text(){
-  $('#messageArea').html('<textarea id="message"></textarea><button onclick="saveMessage()">Save</button>')
-  mode = 'text';
-}
-
-function newMessage(data){
-  console.log(data)
-
-  var marker = new google.maps.Marker({
-    position: data.location,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    icon:'http://i.imgur.com/UYAVMth.png'
-  });
-
-  if(data.type == 'text'){
-  var infoWindow = new google.maps.InfoWindow({
-    content: _.escape(data.message).split('\n').join('</br>')
-  });
-} else {
-    var infoWindow = new google.maps.InfoWindow({
-      content: '<img src="'+data.message+'">'
-    });
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
   }
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
-  });
 
-  $('#log').append('<p onclick="centerMap({lat:'+data.location.lat+',lng:'+data.location.lng+'})"><span>lat '+data.location.lat+' lng '+data.location.lng+'</span>'+_.escape(data.message).split('\n').join('</br>')+'</p>')
-}
+  function saveMessage(){
+    var pos = myPos
+    if(mode =='text'){
+      myFirebaseRef.push({
+        type:'text',
+        message: _.escape(document.getElementById('message').value),
+        location: pos
+      });
+    } else {
+      imageData = $('#simple_sketch')[0].toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, "");
+      $.ajax({
+        url: 'https://api.imgur.com/3/image',
+        headers: {
+          'Authorization': 'Client-ID a2bb58386e94eb0'
+        },
+        type: 'POST',
+        data: {
+          'image': imageData
+        },
+        success: function(data) { myFirebaseRef.push({
+          type:'image',
+          message: data.data.link,
+          location: pos
+        }); }
+      });
+    }
+    myMarker.setMap(null)
+  }
+  function draw(){
+    $('#messageArea').html('<canvas id="simple_sketch" width="300" height="100"></canvas><button onclick="saveMessage()">Save</button>')
+    $(function() {
+      $('#simple_sketch').sketch();
+    });
+    mode = 'draw';
+  }
+  function text(){
+    $('#messageArea').html('<textarea id="message"></textarea><button onclick="saveMessage()">Save</button>')
+    mode = 'text';
+  }
 
-function centerMap(location){
-  map.setCenter(location);
-}
+  function newMessage(data){
+    console.log(data)
+
+    var marker = new google.maps.Marker({
+      position: data.location,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon:'http://i.imgur.com/UYAVMth.png'
+    });
+
+    if(data.type != 'image'){
+      var infoWindow = new google.maps.InfoWindow({
+        content: _.escape(data.message).split('\n').join('</br>')
+      });
+    } else {
+      var infoWindow = new google.maps.InfoWindow({
+        content: '<img src="'+data.message+'">'
+      });
+    }
+    marker.addListener('click', function() {
+      infoWindow.open(map, marker);
+    });
+
+    $('#log').append('<p onclick="centerMap({lat:'+data.location.lat+',lng:'+data.location.lng+'})"><span>lat '+data.location.lat+' lng '+data.location.lng+'</span>'+_.escape(data.message).split('\n').join('</br>')+'</p>')
+  }
+
+  function centerMap(location){
+    map.setCenter(location);
+  }
